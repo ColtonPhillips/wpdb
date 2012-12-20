7571c3219e69f1b9174087fa89e0858f058b9835
Colton Phillips <coltonjphillips@gmail.com>
2012-12-20 06:10:18 -0800

    adding dirtree module for enhanced dirring. needs lots of work.

---
+++ src/dirtree.py
@@ -0,0 +1,23 @@
+import string
+
+#It irks me greatly how my dir is just as useless as dir right now. sasuke it all away.
+
+def __dirt_recurse__(module, verbose=False, file=False, tab_width=0):
+    list = dir(module)
+    if file:
+        # TODO: put that shit in a file
+        pass
+    else:
+        for item in list:
+            if item.startswith("__") and not verbose:
+                pass
+            else:
+                #TODO: I'll need to rethink the recursive stuff when i can actually get a hook on when to stop recursing _ CP
+                print "|" + ("-" * tab_width) + "> " + item
+                # FUUCK IT ILL JUST LIMIT IT AT LIKE 2 OR WHATEVER
+                # Pretty fucking useless, but serves as a demonstration
+                #if tab_width < 2:
+                #   __dirt_recurse__(item,verbose,file,tab_width+1)
+                 
+def dirt(module, verbose=False, file=False):
+    __dirt_recurse__(module, verbose, file)
