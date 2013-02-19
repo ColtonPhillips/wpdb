45081cf60c94da9055bae763909e02d3b7b8a94d
Colton Phillips <coltonjphillips@gmail.com>
2013-02-18 18:28:39 -0800

    decided my user story

---
+++ src/userstory.py
@@ -0,0 +1,20 @@
+# This is a user story hack jam by Colton Phillips for Pascal Courty.
+# The location is Tim Hortons in Victoria BC
+
+# User story is:
+
+QUEST = """I want to run your script and take my list of articles 
+so I can make speculations about how people organize themselves
+within wikipedia"""
+
+import urllib
+
+def user_story():
+    return False
+
+def main():
+    if (user_story == False):
+        print QUEST
+    
+if __name__ == "__main__":
+    main()
