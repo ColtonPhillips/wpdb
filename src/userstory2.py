171291aca1074230dafaad9e6706bfddce8abbd0
Colton Phillips <coltonjphillips@gmail.com>
2013-02-27 05:15:54 -0800

    added second user story

---
+++ src/userstory2.py
@@ -0,0 +1,18 @@
+# This is a user story hack jam by Colton Phillips for Pascal Courty.
+# The location is Tim Hortons in Victoria BC 
+# FEB 18 2013
+
+# A User story is a
+QUEST = """I want to run your script and take my list of articles
+to generate a CSV file populated with the properties I have given you."""
+
+
+def user_story():
+    return False
+
+def main():
+    if (user_story() == False):
+        print QUEST
+    
+if __name__ == "__main__":
+    main()
