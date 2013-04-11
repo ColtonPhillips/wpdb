f807d8e4b8ce8302803d9b00edb1cb61a8d05332
Colton Phillips <coltonjphillips@gmail.com>
2013-04-11 03:38:38 -0700

    created csv to list function and tested woth doctest

--- src/wpdb/__init__.py
+++ src/wpdb/__init__.py
@@ -3,3 +3,6 @@ __all__ = ['wpdb', 'debug', 'wikiurl']
 
 # importing wpdb imports all associated sub-modules as well
 import debug, wikiurl
+
+# we want all of  our core functions over at wpdb.py
+from wpdb import *
--- src/wpdb/wpdb.py
+++ src/wpdb/wpdb.py
@@ -1,12 +1,24 @@
-import MySQLdb
-import sys
-
-def main():
+def csv_file_to_list(csv_file_path):
+    """Converts a csv file_path to a list of strings
+    >>> csv_file_to_list("test/csv_file_to_list_test.csv")
+    ['Pascal', 'Is', 'Lacsap']
+    """
+    import csv
+    out_list = []
+    with open(csv_file_path, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvreader:
+            out_list.append(', '.join(row))
+    return out_list   
+    
+def _main():
     import urllib
     xml = urllib.urlopen("http://stats.grok.se/en/latest30/A_Dangerous_Path")
     
     file = open ("out.txt", 'w')
-    file.write(str(xml.read()))
+    #file.write(str(xml.read()))
 
 if __name__ == "__main__":
-    main()
+    import doctest
+    doctest.testmod()
+    _main()
