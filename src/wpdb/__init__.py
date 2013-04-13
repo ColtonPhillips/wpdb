c570366324e19a73b6dbec5e7097c103dcc0a68f
Colton Phillips <coltonjphillips@gmail.com>
2013-04-13 11:22:36 -0700

    forgot to change the init file along with it

--- src/wpdb/__init__.py
+++ src/wpdb/__init__.py
@@ -1,8 +1,8 @@
 # from wpdb import * will import the following
-__all__ = ['wpdb', 'debug', 'wikiurl']
+__all__ = ['wpdb', 'debug', 'front', 'middle', 'back']
 
 # importing wpdb imports all associated sub-modules as well
-import debug, wikiurl
+import debug, front, middle, back
 
 # we want all of  our core functions over at wpdb.py
 from wpdb import *
